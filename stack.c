bdc585640e98f6ff8fe7184a7f1a113c7f4a2a69
aaronnewland <aaron.newland@rutgers.edu>
2023-02-09 20:16:19 -0500

    Added core functionality to stack.c.

--- stack.c
+++ stack.c
@@ -6,12 +6,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//TODO: DELETE MACROS
+#define DEBUG 0
+
 /* Part 1 - Step 1 and 2: Do your tricks here
  * Your goal must be to change the stack frame of caller (main function)
  * such that you get to the line after "r2 = *( (int *) 0 )"
  */
 void signal_handle(int signalno) {
+    int* temp;
+    temp = &signalno;
 
+    if (DEBUG) {
+        printf("address: %p\n", temp);
+        printf("val1: %u\n", *temp);
+    }
+    temp += 15;
+    *temp += 2;
+    if (DEBUG) {
+        printf("address: %p\n", temp);
+        printf("val: %X\n", *temp);
+    }
     printf("handling segmentation fault!\n");
 
     /* Step 2: Handle segfault and change the stack*/
@@ -20,7 +35,7 @@ void signal_handle(int signalno) {
 int main(int argc, char *argv[]) {
 
     int r2 = 0;
-q
+
     /* Step 1: Register signal handler first*/
     signal(SIGSEGV, signal_handle);
 
